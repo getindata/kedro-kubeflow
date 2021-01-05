0df4b3efe69a36a401c3abf4babc9bfe03eb73c3
Mateusz Pytel <mateusz@getindata.com>
2021-01-05 17:12:04 +0100

    Fix quality issues

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 """kedro_kubeflow module."""
 
 from setuptools import find_packages, setup
-import os
+
 with open("README.md") as f:
-    readme = f.read()
+    README = f.read()
 
 # Runtime Requirements.
-install_requires = ["kedro>=0.16,<=0.18", "click", "kfp", "tabulate"]
+INSTALL_REQUIRES = ["kedro>=0.16,<=0.18", "click", "kfp", "tabulate"]
 
 # Dev Requirements
-extra_require = {
+EXTRA_REQUIRE = {
     "test": ["pytest", "pytest-cov"],
     "dev": ["pytest", "pytest-cov", "pre-commit"],
 }
@@ -18,7 +18,7 @@ setup(
     name="kedro-kubeflow",
     version="0.1.8",
     description="Kedro plugin with Kubeflow support",
-    long_description=readme,
+    long_description=README,
     long_description_content_type="text/markdown",
     license="Apache Software License (Apache 2.0)",
     python_requires=">=3",
@@ -29,13 +29,13 @@ setup(
     ],
     keywords="kedro kubeflow plugin",
     author=u"Mateusz Pytel, Mariusz Strzelecki",
-    author_email="mateusz@getindata.com, mariusz.strzelecki@getindata.com",
+    author_email="mateusz@getindata.com",
     url="https://github.com/getindata/kedro-kubeflow/",
     packages=find_packages(exclude=["ez_setup", "examples", "tests", "docs"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
-    extras_require=extra_require,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRA_REQUIRE,
     entry_points={
         "kedro.project_commands": ["kubeflow = kedro_kubeflow.plugin:commands"],
         "kedro.global_commands": ["kubeflow = kedro_kubeflow.plugin:commands"]
