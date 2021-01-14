3f8d0bdb1ca386668c8df5ebd241271e1095714b
Mateusz Pytel <mateusz@getindata.com>
2021-01-14 23:40:54 +0100

    Logging changes

--- kedro_kubeflow/auth.py
+++ kedro_kubeflow/auth.py
@@ -18,15 +18,15 @@ class AuthHandler(object):
         jwt_token = None
 
         if not client_id:
-            self.log.info(
+            self.log.debug(
                 "No IAP_CLIENT_ID provided, skipping custom IAP authentication"
             )
             return jwt_token
 
         try:
-            self.log.debug("Obtaining JWT token for %s." + client_id)
+            self.log.debug("Attempt to get IAP token for %s." + client_id)
             jwt_token = id_token.fetch_id_token(Request(), client_id)
-            self.log.info("Obtained JWT token for MLFLOW connectivity.")
+            self.log.info("Obtained JWT token for IAP proxy authentication.")
         except DefaultCredentialsError as ex:
             self.log.warning(
                 str(ex)
