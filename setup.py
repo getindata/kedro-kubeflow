a31349f798beef92aa66a6c3945b9a10704121c0
Michał Żelechowski <michal.zelechowski@getindata.com>
2021-05-25 10:35:54 +0200

    Try upgrade of tox.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ EXTRA_REQUIRE = {
     "tests": [
         "pytest>=5.4.0, <7.0.0",
         "pytest-cov>=2.8.0, <3.0.0",
-        "tox==3.21.1",
+        "tox==3.23.1",
         "pre-commit==2.9.3",
     ],
     "docs": [
