8417d65b9aeb17a61c80bf8275d9ac5b42f402a4
Blazej Palkus <blazej.palkus@getindata.com>
2022-07-29 11:39:10 +0200

    fix: Excluded test one import

--- tests/test_generator_utils.py
+++ tests/test_generator_utils.py
@@ -5,8 +5,17 @@ import unittest
 from kedro_kubeflow.generators.utils import is_local_fs
 
 
+def import_condition():
+    try:
+        import gcsfs  # NOQA
+
+        return False
+    except ImportError:
+        return True
+
+
+@unittest.skipIf(import_condition(), "Package gcsfs is not installed")
 class TestGeneratorUtils(unittest.TestCase):
     def test_is_local(self):
-
         assert is_local_fs("data/test/file.txt") is True
         assert is_local_fs("gs://test-bucket/file.txt") is False
