e3fe91dd30ebd1ba4b42ae2a0bbb63fe20e82456
Mariusz Strzelecki <szczeles@gmail.com>
2022-09-22 13:18:21 +0200

    Use the _experiment subfield

--- kedro_kubeflow/cli.py
+++ kedro_kubeflow/cli.py
@@ -324,7 +324,7 @@ def mlflow_start(ctx, kubeflow_run_id: str, output: str):
         raise click.ClickException("Could not read MLFlow config")
 
     run = mlflow.start_run(
-        experiment_id=mlflow_conf.tracking.experiment.experiment_id,
+        experiment_id=mlflow_conf.tracking.experiment._experiment.experiment_id,
         nested=False,
     )
     mlflow.set_tag("kubeflow_run_id", kubeflow_run_id)
